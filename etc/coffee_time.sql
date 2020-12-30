c6a15c3ba0a53c0908920f515e8fa102b091d14d
hasbiak <hasbialwi70@gmail.com>
2020-12-31 03:18:31 +0700

    Update Mysql

--- etc/coffee_time.sql
+++ etc/coffee_time.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Des 2020 pada 20.56
+-- Waktu pembuatan: 30 Des 2020 pada 21.17
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.2.32
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `coffetime`
+-- Database: `coffee_time`
 --
 
 -- --------------------------------------------------------
